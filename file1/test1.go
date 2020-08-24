cdfda277ee45d833a8626a29724e961892dc1a89
repos2 <repos2>
2020-08-24 18:30:33 +0800

    merge commit

--- file1/test1.go
+++ file1/test1.go
@@ -3,7 +3,5 @@ package file1
 func test1()  {
 	print("second file1-test.go  merge!!")
 	print("first branch!!!!")
-	print("second file1-test.go")
-	print("second branch!!!!")
-
+	print("first branch!!!!")
 }
